7c5a9026006831fd56df761dc0054d33372cd137
Jon Szeto <jonszeto821@gmail.com>
2020-10-25 21:04:07 -0400

    lab: fundamentals - Java Primitive Data Types

--- src/labs_examples/fundamentals/labs/Exercise_04.java
+++ src/labs_examples/fundamentals/labs/Exercise_04.java
@@ -16,6 +16,23 @@ public class Exercise_04 {
         System.out.println("int i is: " + i);
 
         // write your code below
+        boolean bool = true;
+        char charVal = 'j';
+        byte byteVal = 12;
+        short shortVal = 111;
+        int intVal = 10000;
+        long longVal = 321232122L;
+        float floatVal = 2343.11F;
+        double doubleVal = 432423.32423432423423;
+
+        System.out.println(bool);
+        System.out.println(charVal);
+        System.out.println(byteVal);
+        System.out.println(shortVal);
+        System.out.println(intVal);
+        System.out.println(longVal);
+        System.out.println(floatVal);
+        System.out.println(doubleVal);
 
     }
 
